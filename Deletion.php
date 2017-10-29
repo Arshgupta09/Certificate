e3f7ab4e5c7ea3dcbf38f78ca51b4dd63fecf205
Arsh Gupta <30995754+Arshgupta09@users.noreply.github.com>
2017-10-29 13:47:17 +0530

    Update Deletion.php

--- Deletion.php
+++ Deletion.php
@@ -3,15 +3,18 @@ $days =7;
 $path ="odt/base/";
 
 // Open the directory
+
 if ($handle = opendir($path))
-{
-    // Loop through the directory
+{ // Loop through the directory
     while (false !== ($file = readdir($handle)))
     {
         // Check the file we're doing is actually a file
         if (is_file($path.$file))
         {
-            // Check if the file is older than X days old
+          $ext = pathinfo($file, PATHINFO_EXTENSION);
+         if ($ext == "odt")
+         {
+         // Check if the file is older than X days old
             if (filemtime($path.$file) < ( time() - ( $days * 24 * 60 * 60 ) ) )
             {
                 // Do the deletion
@@ -19,5 +22,6 @@ if ($handle = opendir($path))
             }
         }
     }
+}
 }
 ?>
